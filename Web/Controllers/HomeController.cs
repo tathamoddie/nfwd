1bfbc3822838234cdebfb49206ae96d025c498c4
Tatham Oddie <tatham@oddie.com.au>
2012-01-02 20:05:18 +1100

    Add homepage stub.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
